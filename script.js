5e4001dd35c2a5660af8ee94d2a428375d62a96a
Grecu Liviu <110229478+Liviu-sourceCode@users.noreply.github.com>
2023-08-30 15:23:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+let dropdown = document.querySelector('.dropdown-content');
+let dropbutton = document.querySelector('.dropbtn');
+let bars = document.querySelector('#bars');
+let xmark = document.querySelector('#mark');
+
+dropdown.style.display = "none";
+xmark.style.display = "none";
+
+
+dropbutton.addEventListener('click', (e) => {
+
+    
+
+    if(dropdown.style.display == "none") {
+        
+        xmark.style.display = "block";
+        bars.style.display = "none";
+        dropdown.style.display = "block";
+        dropbutton.style.background = "#1d1d24";
+    
+    } else {
+
+       
+        xmark.style.display = "none";
+        bars.style.display = "block";
+        dropdown.style.display = "none";
+        dropbutton.style.background = "none";
+    }
+
+  
+
+})
+
+
+
+
+     
